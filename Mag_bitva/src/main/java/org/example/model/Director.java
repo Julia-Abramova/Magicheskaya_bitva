bfe4602b1c43faa98e24e747695cd288c5672de6
Julia-Abramova <yulia.abramova14@yandex.ru>
2026-05-16 00:24:59 +0300

    Update Director.java

--- Mag_bitva/src/main/java/org/example/model/Director.java
+++ Mag_bitva/src/main/java/org/example/model/Director.java
@@ -4,7 +4,10 @@ package org.example.model;
 public class Director {
     private MissionBuilder missionBuilder;
     
-    public void setMissionBuilder(MissionBuilder mb) {}
+    public void setMissionBuilder(MissionBuilder mb) {missionBuilder = mb; }
+    public Mission getMission() {return missionBuilder.getMission(); }
     
-    
+    public void constructMission() {
+        
+    }
 }
